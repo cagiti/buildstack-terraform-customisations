5edfbb48b4ac3fa52976dbb747b8007170497fd1
Cai Cooper <caicooper82@gmail.com>
2018-02-26 15:17:25 +0000

    Corrected project reference and authorized networks

--- database.tf
+++ database.tf
@@ -1,6 +1,8 @@
 module "bosh-db" {
   source = "github.com/migs/terraform-gcp-database"
-  project = "${var.project}"
+  project = "${var.project_id}"
   region = "${var.region}"
   authorized_network_0 = "${module.terraform-gcp-natgateway.nat-gateway-ips["0"]}"
+  authorized_network_1 = "${module.terraform-gcp-natgateway.nat-gateway-ips["0"]}"
+  authorized_network_2 = "${module.terraform-gcp-natgateway.nat-gateway-ips["0"]}"
 }
